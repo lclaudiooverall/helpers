98d221f874c01e0890d13814479487b68e683611
Luis Augusto Claudio Ponce <1107000044@upci.edu.pe>
2021-02-11 02:39:57 -0500

    Create jquery_checked.js

---
+++ jquery_checked.js
@@ -0,0 +1,9 @@
+$(".micheckbox").on( 'change', function() {
+    if( $(this).is(':checked') ) {
+        // Hacer algo si el checkbox ha sido seleccionado
+        alert("El checkbox con valor " + $(this).val() + " ha sido seleccionado");
+    } else {
+        // Hacer algo si el checkbox ha sido deseleccionado
+        alert("El checkbox con valor " + $(this).val() + " ha sido deseleccionado");
+    }
+});
